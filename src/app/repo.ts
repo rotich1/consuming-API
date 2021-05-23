bbd9fb8dcbf8e36c11f60ebd1a5a32a7f2bf3417
Softonic254 <rotichenoch27@gmail.com>
2021-05-23 18:33:47 +0300

    It will add create_at date

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,9 +1,11 @@
 export class Repo {
     constructor(
-        public name:string,
-        public description:string,
-        public updated_at:Date,
+        public name: string,
+        public description: string,
+        public updated_at: Date,
         public clone_link: string,
-        public language:string
+        public language: string,
+        public html_url: string,
+        public created_at: Date
     ) { }
 }
