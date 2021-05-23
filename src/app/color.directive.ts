166c0715d939ab08cb6d29cf639c969c66113744
Softonic254 <rotichenoch27@gmail.com>
2021-05-23 22:50:57 +0300

    It will create a component for not-found

---
+++ src/app/color.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appColor]'
+})
+export class ColorDirective {
+
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.style.color = "blue";
+    this.element.nativeElement.style.textTransform= "uppercase"
+    this.element.nativeElement.style.fontFamily = "serif"
+    this.element.nativeElement.style.weight = "700"
+  }
+
+}
