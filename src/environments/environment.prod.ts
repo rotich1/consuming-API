af3ad607b18a67993edcfc33d4d6101b743a3c70
Softonic254 <rotichenoch27@gmail.com>
2021-05-24 18:46:59 +0300

    It will remove link from environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  apiUrl1 : `https://api.github.com/users/username/repos`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,6 @@
 
 export const environment = {
   production: true,
-  apiUrl1 : `https://api.github.com/users/username/repos`
-
 };
 
 /*
